64bce25d7054f03d1d68c5c638669595edc7c6ac
y3etboi69 <harish_ram@s2021.ssts.edu.sg>
2022-07-09 11:03:50 +0800

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by HARISH RAM BAGHAVATH stu on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
